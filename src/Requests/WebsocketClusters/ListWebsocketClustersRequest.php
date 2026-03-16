d2b41c82cc83371909cdea674c8e0390a356bce1
Gaga Darsalia <gaga@redberry.ge>
2026-03-16 10:05:58 +0400

    feat: add ListWebsocketClustersRequest and corresponding tests for listing websocket clusters in Laravel Cloud SDK

---
+++ src/Requests/WebsocketClusters/ListWebsocketClustersRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Integrations\LaravelCloud\Requests\WebsocketClusters;
+
+use App\Data\LaravelCloud\WebsocketClusters\WebsocketClusterData;
+use Illuminate\Support\Collection;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+
+class ListWebsocketClustersRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function resolveEndpoint(): string
+    {
+        return '/websocket-servers';
+    }
+
+    /**
+     * @return Collection<int, WebsocketClusterData>
+     */
+    public function createDtoFromResponse(Response $response): Collection
+    {
+        return collect($response->json('data'))
+            ->map(fn (array $item) => WebsocketClusterData::fromResponse($item['attributes'], $item['id']));
+    }
+}
