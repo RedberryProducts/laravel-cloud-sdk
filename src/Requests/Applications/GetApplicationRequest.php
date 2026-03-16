c230bab5ff11ee950e28e4416d56af8c9508e654
Gaga Darsalia <gaga@redberry.ge>
2026-03-16 11:25:08 +0400

    feat: add GetApplicationRequest and corresponding tests for retrieving application data in Laravel Cloud SDK

---
+++ src/Requests/Applications/GetApplicationRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Integrations\LaravelCloud\Requests\Applications;
+
+use App\Data\LaravelCloud\Applications\ApplicationData;
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Saloon\Http\Response;
+
+class GetApplicationRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    public function __construct(private string $applicationId) {}
+
+    public function resolveEndpoint(): string
+    {
+        return "/applications/{$this->applicationId}";
+    }
+
+    public function createDtoFromResponse(Response $response): ApplicationData
+    {
+        $data = $response->json('data');
+
+        return ApplicationData::fromResponse($data['attributes'], $data['id']);
+    }
+}
